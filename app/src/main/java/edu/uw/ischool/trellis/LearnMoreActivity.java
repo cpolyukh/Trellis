163e348c6ac1644d698762741ad242592bd5107f
Eden Ghirmai <edeng94@gmail.com>
2016-04-16 17:26:21 -0700

    added messages, profile, and supporter activity pages and layout skeletons

--- app/src/main/java/edu/uw/ischool/trellis/LearnMoreActivity.java
+++ app/src/main/java/edu/uw/ischool/trellis/LearnMoreActivity.java
@@ -5,24 +5,18 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import edu.uw.ischool.trellis.UI.ToolbarView;
 import android.view.View;
-import android.widget.ImageButton;
-import android.widget.RelativeLayout;
 
-public class SupportSelectionActivity extends AppCompatActivity {
+public class LearnMoreActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_support_selection);
+        setContentView(R.layout.activity_learn_more);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        RelativeLayout toolbar = (RelativeLayout) findViewById(R.id.main);
-       // ImageButton img = (ImageButton) findViewById(R.id.ello);
-
-        //setSupportActionBar(toolbar);
-
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
--- app/src/main/java/edu/uw/ischool/trellis/LoginActivity.java
+++ app/src/main/java/edu/uw/ischool/trellis/LoginActivity.java
@@ -2,38 +2,49 @@ package edu.uw.ischool.trellis;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
+
+import edu.uw.ischool.trellis.UI.ToolbarView;
 
 
-public class LoginActivity extends ActionBarActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.main);
+        ToolbarView toolbar = (ToolbarView) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_login, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 }
