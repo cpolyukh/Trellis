74d636b9f553d0586e6a0710c303927253d4f1f6
Eden Ghirmai <edeng94@gmail.com>
2016-05-01 19:25:06 -0700

    Facebook login functioning

--- app/src/main/java/edu/uw/ischool/trellis/LoginActivity.java
+++ app/src/main/java/edu/uw/ischool/trellis/LoginActivity.java
@@ -1,17 +1,23 @@
 package edu.uw.ischool.trellis;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Base64;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -20,9 +26,8 @@ import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
-
-import java.util.ArrayList;
-import java.util.List;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import edu.uw.ischool.trellis.UI.ToolbarView;
 
@@ -32,38 +37,71 @@ public class LoginActivity extends AppCompatActivity {
     LoginButton loginButton;
     Button guestButton;
     CallbackManager callbackManager;
+    AccessTokenTracker accessTokenTracker;
+    AccessToken accessToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
-        AppEventsLogger.activateApp(this);
+        callbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.activity_login);
 
-        List<String> permissions = new ArrayList<String>();
-        permissions.add("user_friends");
-        permissions.add("public_profile");
-
         loginButton = (LoginButton) findViewById(R.id.login_button);
-        loginButton.setReadPermissions(permissions);
-        callbackManager = CallbackManager.Factory.create();
+        loginButton.setReadPermissions("public_profile", "user_friends");
+
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(
+                    AccessToken oldAccessToken,
+                    AccessToken currentAccessToken) {
+                // Set the access token using
+                // currentAccessToken when it's loaded or set.
+                accessToken = currentAccessToken;
+            }
+        };
+
+        // If the access token is available already assign it.
+        accessToken = AccessToken.getCurrentAccessToken();
+
+        //generate keyhash
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "edu.uw.ischool.trellis",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
 
         // Callback registration
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                Intent next = new Intent(LoginActivity.this, SupportSelectionActivity.class);
-                startActivity(next);
+                // App code
+                Toast.makeText(getApplicationContext(), "Login success", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(LoginActivity.this, SupportSelectionActivity.class));
 
             }
 
             @Override
-            public void onCancel() { }
+            public void onCancel() {
+                // App code
+                Toast.makeText(getApplicationContext(),"Login cancelled",Toast.LENGTH_SHORT).show();
+            }
 
             @Override
-            public void onError(FacebookException exception) { }
+            public void onError(FacebookException exception) {
+                // App code
+                Toast.makeText(getApplicationContext(),exception.getMessage(), Toast.LENGTH_SHORT).show();
+            }
         });
-
 
         guestButton = (Button) findViewById(R.id.guestButton);
         guestButton.setOnClickListener(new View.OnClickListener() {
@@ -79,5 +117,36 @@ public class LoginActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Logs 'install' and 'app activate' App Events.
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
