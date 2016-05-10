acfbeb041b28359529d6d6ecbdf4fece114d6015
Eden Ghirmai <edeng94@gmail.com>
2016-05-10 09:37:05 -0700

    hallelujah images show up in supporter view

--- app/src/main/java/edu/uw/ischool/trellis/SupporterArrayAdapter.java
+++ app/src/main/java/edu/uw/ischool/trellis/SupporterArrayAdapter.java
@@ -1,9 +1,11 @@
 package edu.uw.ischool.trellis;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +13,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -30,6 +33,10 @@ import java.nio.charset.MalformedInputException;
 public class SupporterArrayAdapter extends ArrayAdapter<Supporter> {
     private final Context context;
     private final Supporter[] values;
+    private ProgressDialog pDialog;
+    private ImageView iv;
+    private Bitmap bitmap;
+
 
     public SupporterArrayAdapter(Context context, Supporter[] values) {
         super(context, -1, values);
@@ -38,65 +45,32 @@ public class SupporterArrayAdapter extends ArrayAdapter<Supporter> {
     }
 
     public View getView(int position, View convertView, ViewGroup parent) {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View rowView = inflater.inflate(R.layout.supporter_results_row, parent, false);
         TextView nameTextView = (TextView) rowView.findViewById(R.id.txtSupporterName);
         TextView countTextView = (TextView) rowView.findViewById(R.id.txtSupportCount);
         TextView quoteTextView = (TextView) rowView.findViewById(R.id.txtQuote);
-        ImageView imageView = (ImageView) rowView.findViewById(R.id.supportIcon);
+        iv = (ImageView) rowView.findViewById(R.id.supportIcon);
 
         nameTextView.setText(values[position].getName());
         countTextView.setText("" + values[position].getSupporteeCount());
         quoteTextView.setText(values[position].getQuote());
-        //TODO: set the image to something other than the default icon
 
-//        String imgURL = "http://graph.facebo" +
-//                "ok.com/" + values[position].getId() + "/picture";
-//
-//        try {
-//            URL url = new URL(imgURL);
-//            Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-//            imageView.setImageBitmap(bmp);
-//            Log.e("Create Bitmap", "--------------------" + bmp.toString());
-//        } catch (IOException e) {
-//            Log.e("Image Download", e.getMessage());
-//        }
+        // Set user image
+        String imgURL = "https://graph.facebook.com/" + values[position].getId() + "/picture?type=large";
 
-
-
-        //new ImageLoadTask(url, imageView).execute();
-        //new DownloadImageTask(imageView).execute(url);
-
+        try {
+            URL url = new URL(imgURL);
+            Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+            iv.setImageBitmap(bmp);
+        } catch (IOException e) {
+            Log.e("Image Download", e.getMessage());
+        }
 
         return rowView;
-    }
-
-    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
-        ImageView bmImage;
-
-        public DownloadImageTask(ImageView bmImage) {
-            this.bmImage = bmImage;
-        }
-
-        protected Bitmap doInBackground(String... urls) {
-            String urldisplay = urls[0];
-            Bitmap mIcon11 = null;
-            try {
-                InputStream in = new java.net.URL(urldisplay).openStream();
-                mIcon11 = BitmapFactory.decodeStream(in);
-                in.close();
-            } catch (Exception e) {
-                Log.e("Error", e.getMessage());
-                e.printStackTrace();
-            }
-
-            Log.e("Create Bitmap",  "--------------------" + mIcon11.toString());
-            return mIcon11;
-        }
-
-        protected void onPostExecute(Bitmap result) {
-            bmImage.setImageBitmap(result);
-        }
     }
 }
 
